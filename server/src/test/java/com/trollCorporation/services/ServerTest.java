8608f101c5a541f880a30d1884e919e2a4ac4ef4
madgaet <madgaet@gmail.com>
2014-12-22 21:56:21 +0100

    test changes

---
+++ server/src/test/java/com/trollCorporation/services/ServerTest.java
@@ -0,0 +1,20 @@
+package com.trollCorporation.services;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class ServerTest {
+	
+	Server server;
+	
+	@Before
+	public void setUp() {
+		server = Mockito.mock(Server.class);
+	}
+	
+	@Test
+	public void testServerMethod() {
+		//I don't know what to test
+	}
+}
